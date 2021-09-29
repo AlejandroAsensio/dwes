7e1755c5beef3c54436c713bf564c36438c59546
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2021-09-29 14:21:30 +0200

    Se fue el otro dia, hoy no volvio

---
+++ t2/ejercicios/ejInventCl.php
@@ -0,0 +1,9 @@
+<form>
+<input type="radio" id ="uno"name="numeros" value="1"/>
+<label for="uno">1</label><br/>
+<input type="radio" id ="dos"name="numeros" value="2"/>
+<label for="dos">2</label><br/>
+<input type="radio" id ="tres"name="numeros" value="3"/>
+<label for="tres">3</label><br/>
+<input type="submit"/>
+</form>
