89278b217f68d32f596fff6eea283a27df32b6be
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2021-10-05 21:26:32 +0200

    Pruebas en casa

--- t3/ejercicios/ej2.php
+++ t3/ejercicios/ej2.php
@@ -1,11 +1,16 @@
 <?php
 $condicion = ! isset($_COOKIE['nVisita']);
-
+$numVeces = $_COOKIE['nVisita'];
 if ($condicion) {
     setcookie('nVisita', 2);
     header('Prueba:Cabezon');
     echo "<h1>Bienvenido</h1>";
-} else {
+}
+else if($numVeces==26){
+    echo "<h1>Eres tonto</h1>";
+    setcookie('nVisita', $numVeces + 1);
+}
+else {
     $numVeces = $_COOKIE['nVisita'];
     setcookie('nVisita', $numVeces + 1);
     echo "<h1>Hola es la vez numero $numVeces que visitas esta pagina";
